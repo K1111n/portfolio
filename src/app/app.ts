e577956e1ee69b58916a97490eed241c3c8c9483
K1111n <kaja_nan@hotmail.de>
2025-09-28 12:02:46 +0200

    fix: all components "works!"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Hero } from "./hero/hero";
+import { AboutMe } from "./about-me/about-me";
+import { Skills } from "./skills/skills";
+import { Portfolio } from "./portfolio/portfolio";
+import { References } from "./references/references";
+import { Contact } from "./contact/contact";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Hero, AboutMe, Skills, Portfolio, References, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
