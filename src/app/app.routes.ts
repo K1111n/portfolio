a955383404d0d6923e64180acbdafb29d908209e
K1111n <kaja_nan@hotmail.de>
2025-10-14 22:29:09 +0200

    feat: privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Impressum } from './impressum/impressum';
+import { PrivacyPolicy } from './privacy-policy/privacy-policy';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'impressum', component: Impressum },
+  { path: 'privacy-policy', component: PrivacyPolicy },
+];
---
+++ src/app/impressum/impressum.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-impressum',
+  imports: [RouterLink],
+  templateUrl: './impressum.html',
+  styleUrls: ['./impressum.scss']
+})
+export class Impressum {}
