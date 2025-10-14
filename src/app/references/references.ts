ad02a711250951b57b9bcb3e05372aba5d313fe3
K1111n <kaja_nan@hotmail.de>
2025-10-14 09:40:29 +0200

    fix: line in contact moved

--- src/app/references/references.ts
+++ src/app/references/references.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 interface Testimonial {
@@ -15,36 +15,27 @@ interface Testimonial {
 })
 export class References {
   currentIndex = 0;
-  autoPlayInterval: any;
 
   testimonials: Testimonial[] = [
     {
       text: "This is an amazing service! Highly recommended.",
       author: "John Doe",
-      image: ""
+      image: "/no-profile-pic.png"
     },
     {
       text: "A wonderful experience from start to finish.",
       author: "Jane Smith",
-      image: ""
+      image: "/no-profile-pic.png"
     },
     {
       text: "Professional and efficient, couldn't ask for more.",
       author: "Alice Johnson",
-      image: ""
+      image: "/no-profile-pic.png"
     }
   ];
 
   get currentTestimonial(): Testimonial {
     return this.testimonials[this.currentIndex];
-  }
-
-  ngOnInit() {
-    this.startAutoPlay();
-  }
-
-  ngOnDestroy() {
-    this.stopAutoPlay();
   }
 
   nextSlide() {
@@ -58,18 +49,4 @@ export class References {
   goToSlide(index: number) {
     this.currentIndex = index;
   }
-
-  startAutoPlay() {
-    this.autoPlayInterval = setInterval(() => {
-      this.nextSlide();
-    }, 5000);
-  }
-
-  stopAutoPlay() {
-    if (this.autoPlayInterval) {
-      clearInterval(this.autoPlayInterval);
-    }
-  }
 }
-
-
