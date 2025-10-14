26dba90fd05f857d630fd5fff708d941245dc3b0
K1111n <kaja_nan@hotmail.de>
2025-10-14 23:17:45 +0200

    feat: burger menu

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrls: ['./header.scss']
 })
 export class Header {
+  menuOpen = false;
+  currentLanguage = 'en';
 
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+    
+    if (this.menuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  switchLanguage(lang: string): void {
+    this.currentLanguage = lang;
+    console.log('Language switched to:', lang);
+    
+    if (this.menuOpen) {
+      this.toggleMenu();
+    }
+  }
 }
