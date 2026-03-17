0f53b1e9ae8321905dda6776b58943b3ccfe09da
Kajanan Yoganathan <kaja_nan@hotmail.de>
2026-03-17 15:23:11 +0100

    fix: scrollTo leads to the top of page

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -69,5 +69,7 @@ export class Contact {
 
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
+    document.body.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
